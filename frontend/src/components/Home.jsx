25f530714aa616b0b9bf47811af404947b791755
Chandan-Gowda-Kalvium <chandan.sm@kalvium.community>
2023-02-27 09:35:45 +0530

    Minnor changes in CSS

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -13,6 +13,9 @@ function Home() {
             <h3 className='heading-content'>The beauty of the game is strength stamina and technique and when you shorten the game, technique goes for a toss and the quality of badminton will surely go down in this system.</h3>
             <div className='buttons'>
                 <Link to={'/login'}><button className='login'>LogIn</button></Link>
+                
+                <Link to={'/gamepage'}><button className='login'>skip</button></Link>
+
                 <Link to={'/signup'}><button className='signup'>SignUp</button></Link>
             </div>
         </div>
