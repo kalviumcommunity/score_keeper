8deae35088c5ddebfc4a638889f18d6bbac81189
2005balaji <balajisr000@gmail.com>
2023-02-28 22:40:57 +0530

    Storing the Title

--- frontend/src/components/NewGame.jsx
+++ frontend/src/components/NewGame.jsx
@@ -1,71 +1,97 @@
-import React, { useState } from 'react'
-import Court from '../Pictures/court.png'
-import Button from './Button';
-import TextField from '@mui/material/TextField'
+import React, { useState, useEffect } from "react";
+import Court from "../Pictures/court.png";
+import Button from "./Button";
+import TextField from "@mui/material/TextField";
 // import SemiResult from './SemiResult';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 function NewGame() {
+  //title of game
 
-    // Logic 
+  const [inputValue, setInputValue] = useState("");
+  console.log(inputValue);
 
-    const [count1, setCount1] = useState(0);
-    const navigate = useNavigate();
+  // Update session storage whenever the input value changes
+  useEffect(() => {
+    sessionStorage.setItem("inputValue", inputValue);
+  }, [inputValue]);
 
-    let incrementCountP1 = () => {
-        setCount1(count1 + 1);
-    };
-    
+  // Update state whenever the input value changes
+  const handleInputChange = (event) => {
+    setInputValue(event.target.value);
+  };
 
-    let undoCount1 = () => {
-        setCount1(count1 - 1);
-    };
+  // Logic
 
-    const [count2, setCount2] = useState(0);
-    let incrementCountP2 = () => {
-        setCount2(count2 + 1);
-    };
-    let undoCount2 = () => {
-        
-        setCount2(count2 - 1);
-    };
+  const [count1, setCount1] = useState(0);
+  const navigate = useNavigate();
 
-    if(count1 >= 21)
-    {
-        navigate('/semiresult')
-    }
+  let incrementCountP1 = () => {
+    setCount1(count1 + 1);
+  };
+
+  let undoCount1 = () => {
+    setCount1(count1 - 1);
+  };
+
+  const [count2, setCount2] = useState(0);
+  let incrementCountP2 = () => {
+    setCount2(count2 + 1);
+  };
+  let undoCount2 = () => {
+    setCount2(count2 - 1);
+  };
+
+  if (count1 >= 21) {
+    navigate("/semiresult");
+  }
+  
 
   return (
-
-    <div className='newgame-page'>
-
-        <>
-        <input id='gametitle-re' type="text" placeholder="This is the Game Title !!" />
-        </>
-        <div className='main-part-up'>
-            <div className='score-1'>{count1}</div>
-            <div>
-                <img className='b-court' src={Court} alt="Badminton-Court" />
-            </div>
-            <div className='score-2'>{count2}</div>
+    <div className="newgame-page">
+      <>
+        <input
+          id="gametitle-re"
+          type="text"
+          placeholder="This is the Game Title !!"
+          value={inputValue}
+          onChange={handleInputChange}
+        />
+      </>
+      <div className="main-part-up">
+        <div className="score-1">{count1}</div>
+        <div>
+          <img className="b-court" src={Court} alt="Badminton-Court" />
         </div>
-        <div className='main-part-down'>
-            <div className='p-1-details'>
-                <TextField className='p-1-details-name' id="white-text" label="Player 1" variant="standard" />
-                <div className='p-1-details-score'>{count1}</div>
-            </div>
-            <div className='functions'>
-                <Button className='p-1-add' title={'+1'} action={incrementCountP1}/>
-                <Button title={'undo1'} action={undoCount1}/>
-                <Button title={'undo2'} action={undoCount2}/>
-                <Button title={'+1'} action={incrementCountP2}/>
-            </div>
-            <div className='p-2-details'>
-                <TextField className='p-2-details-name' id="white-text" label="Player 2" variant="standard" />
-                <div className='p-2-details-score'>{count2}</div>
-            </div>
+        <div className="score-2">{count2}</div>
+      </div>
+      <div className="main-part-down">
+        <div className="p-1-details">
+          <TextField
+            className="p-1-details-name"
+            id="white-text"
+            label="Player 1"
+            variant="standard"
+          />
+          <div className="p-1-details-score">{count1}</div>
         </div>
+        <div className="functions">
+          <Button className="p-1-add" title={"+1"} action={incrementCountP1} />
+          <Button title={"undo1"} action={undoCount1} />
+          <Button title={"undo2"} action={undoCount2} />
+          <Button title={"+1"} action={incrementCountP2} />
+        </div>
+        <div className="p-2-details">
+          <TextField
+            className="p-2-details-name"
+            id="white-text"
+            label="Player 2"
+            variant="standard"
+          />
+          <div className="p-2-details-score">{count2}</div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
-export default NewGame
+export default NewGame;
--- frontend/src/components/SemiResult.jsx
+++ frontend/src/components/SemiResult.jsx
@@ -1,34 +1,52 @@
-import React from 'react'
-import Court from '../Pictures/court.png'
+import React, { useState } from "react";
+import Court from "../Pictures/court.png";
 // import Button from './Button';
-import TextField from '@mui/material/TextField'
+import TextField from "@mui/material/TextField";
 
 function SemiResult() {
-    return (
-        <div className='newgame-page'>
-            <TextField className='gametitle-sm' id="outlined-basic" label="This is the Game Title !!" variant="outlined" />
-            <div className='main-part-up-sm'>
-                <div className='score-1'>21</div>
-                <div>
-                    <img className='b-court' src={Court} alt="Badminton-Court" />
-                </div>
-                <div className='score-2'>0</div>
-            </div>
-            <div className='main-part-down-sm'>
-                <div className='p-details'>
-                    <div className='p-1-details'>
-                        <TextField className='p-1-details-name' id="standard-basic" label="Player 1" variant="standard" />
-                        <div className='p-1-details-score'>21</div>
-                    </div>
-                    <div className='p-2-details'>
-                        <TextField className='p-2-details-name' id="standard-basic" label="Player 2" variant="standard" />
-                        <div className='p-2-details-score'>0</div>
-                    </div>
-                </div>
-                <h1 className='win-text'>"Player 1" wins the game with a score of “ 21 / 0 “</h1>
-            </div>    
-        </div>
-      )
-    }
+  let Title = sessionStorage.getItem("inputValue");
 
-export default SemiResult
+//   console.log(Title);
+
+  return (
+    <div className="newgame-page">
+        
+      <h1>{Title}</h1>
+
+      <div className="main-part-up-sm">
+        <div className="score-1">21</div>
+        <div>
+          <img className="b-court" src={Court} alt="Badminton-Court" />
+        </div>
+        <div className="score-2">0</div>
+      </div>
+      <div className="main-part-down-sm">
+        <div className="p-details">
+          <div className="p-1-details">
+            <TextField
+              className="p-1-details-name"
+              id="standard-basic"
+              label="Player 1"
+              variant="standard"
+            />
+            <div className="p-1-details-score">21</div>
+          </div>
+          <div className="p-2-details">
+            <TextField
+              className="p-2-details-name"
+              id="standard-basic"
+              label="Player 2"
+              variant="standard"
+            />
+            <div className="p-2-details-score">0</div>
+          </div>
+        </div>
+        <h1 className="win-text">
+          "Player 1" wins the game with a score of “ 21 / 0 “
+        </h1>
+      </div>
+    </div>
+  );
+}
+
+export default SemiResult;
