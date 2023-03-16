d2960010c3ad3b13aa088e515f386b0c16c2c56d
Chandan-Gowda-Kalvium <chandan.sm@kalvium.community>
2023-03-16 12:44:34 +0530

    changes made as suggested

---
+++ score_keeper_client/src/components/GamePage.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import badminton from "../Pictures/pngegg.png"
+import { Link } from 'react-router-dom'
+
+function GamePage() {
+  return (
+    <div className="game-page">
+        <div className='picture-gp'>
+            <img className='pic' src={badminton} alt="react-app" />
+        </div>
+        <div className="gamepage-content">
+            <Link className='record-game' to={'/newgame'}>Record Game</Link>
+            <Link className='view-game' to={'/oldgames'}>View Old Game</Link>
+        </div>
+    </div>
+  )
+}
+
+export default GamePage
