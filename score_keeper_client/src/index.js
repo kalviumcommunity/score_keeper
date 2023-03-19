6e9f9bf5f66406fbab6a067116a718f7be86e034
Chandan-Gowda-Kalvium <chandan.sm@kalvium.community>
2023-03-19 19:38:50 +0530

    About Page Created

--- score_keeper_client/src/index.js
+++ score_keeper_client/src/index.js
@@ -5,18 +5,15 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom'
 import { Auth0Provider } from "@auth0/auth0-react";
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <Auth0Provider
-    domain="chandan-sm.us.auth0.com"
-    clientId="jgqehxupirFDBCX14UAtLuXzqYLo5wbQ"
+    domain={process.env.REACT_APP_DOMAIN}
+    clientId={process.env.REACT_APP_CLIENT_ID}
     authorizationParams={{redirect_uri: window.location.origin}}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </Auth0Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
