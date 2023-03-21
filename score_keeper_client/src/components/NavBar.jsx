ae614c7a358cc1e36876810324f45863f1297924
Chandan-Gowda-Kalvium <chandan.sm@kalvium.community>
2023-03-21 21:22:31 +0530

    Fetch feature updated and backend Hosted

--- score_keeper_client/src/components/NavBar.jsx
+++ score_keeper_client/src/components/NavBar.jsx
@@ -2,23 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import fevicon from '../Pictures/Logo.png'
 import LoginButton from './Login/LoginButton'
-// import LogoutButton from './LogoutButton'
-// import Profile from './Profile'
 
 
 function NavBar() {
   return (
     <div className="navbar"> 
         <Link to={'/'} className='logo'><img className='logo' src={fevicon} alt="" /></Link>
-        {/* <h1 className='app-name'> Sco<span>re</span> Kee<span>per</span></h1> */}
         <div className='nav-content'>
           <Link to={'/'}><div className='nav-con'>Home</div></Link>
           <Link to={'/about'}><div className='nav-con'>About</div></Link>
           <Link to={'/rules'}><div className='nav-con'>Rules</div></Link>
         </div>
         <LoginButton>LogIn</LoginButton>
-        {/* <LogoutButton id='login'>LogOut</LogoutButton> */}
-        {/* <Profile id='profile'></Profile> */}
     </div>
   )
 }
--- score_keeper_client/src/components/OldGame.jsx
+++ score_keeper_client/src/components/OldGame.jsx
@@ -1,48 +1,71 @@
-import React from 'react'
+import React from "react";
 import badminton from "../Pictures/pngegg.png";
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
 
 function OldGame() {
-
   // Make a GET request to your backend API
-  const [player1, setPlayer1] = useState('');
-  const [player2, setPlayer2] = useState('');
-  const [score1, setScore1] = useState(0);
-  const [score2, setScore2] = useState(0);
-  const [winText, setWinText] = useState('');
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:4000/singleGameData/64145d0bb0a5c251d35886e5')
-      .then(response => response.json())
-      .then(data => {
-        setPlayer1(data.player1);
-        setPlayer2(data.player2);
-        setScore1(data.score1);
-        setScore2(data.score2);
-        setWinText(data.winText);
+    fetch(process.env.REACT_APP_WEB_URI)
+      .then((response) => response.json())
+      .then((data) => {
+        setData(data);
       })
-      .catch(error => console.log(error));
+      .catch((error) => console.log(error));
   }, []);
 
-
   return (
-    <div className="game-page">
-        <div className='picture-gp'>
-            <img className='pic' src={badminton} alt="react-app" />
+    <>
+      <div className="game-page">
+        <div className="picture-gp">
+          <img className="pic" src={badminton} alt="react-app" />
         </div>
         <div className="old-gamepage-content">
-          <h1 className='old-gamepage-heading'>Played Games</h1>
-          <div className='login' style={{marginRight:"0px"}}>
-            <h1 style={{backgroundColor:"transparent", marginTop:"0px", marginBottom:"0px"}}>{player1} vs {player2}</h1>
-            <h2 style={{backgroundColor:"transparent", marginTop:"0px", marginBottom:"0px"}}>Score: {score1} - {score2}</h2>
-            <p style={{backgroundColor:"transparent", marginTop:"5px", marginBottom:"10px"}}>{winText}</p>
+          <h1 className="old-gamepage-heading">Played Games</h1>
+          <div className="old-gamepage-data">
+          {data.map((item, i) => {
+            return (
+              <div
+                className="login"
+                style={{ marginRight: "0px", marginBottom:"1vh", padding: "20px 30px" }}
+                key={i}
+              >
+                <h1
+                  style={{
+                    backgroundColor: "transparent",
+                    marginTop: "0px",
+                    marginBottom: "0px",
+                  }}
+                >
+                  {item.player1} vs {item.player2}
+                </h1>
+                <h2
+                  style={{
+                    backgroundColor: "transparent",
+                    marginTop: "0px",
+                    marginBottom: "0px",
+                  }}
+                >
+                  Score: {item.score1} - {item.score2}
+                </h2>
+                <p
+                  style={{
+                    backgroundColor: "transparent",
+                    marginTop: "5px",
+                    marginBottom: "10px",
+                  }}
+                >
+                  {item.winText}
+                </p>
+              </div>
+            );
+          })}
           </div>
         </div>
-    </div>
-  )
+      </div>
+    </>
+  );
 }
 
-export default OldGame
-
-
-
+export default OldGame;
