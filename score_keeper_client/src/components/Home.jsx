1482146420bf80004f59822b2a360e022af1ab36
Chandan-Gowda-Kalvium <chandan.sm@kalvium.community>
2023-05-23 23:43:13 +0530

    Slight Changes

--- score_keeper_client/src/components/Home.jsx
+++ score_keeper_client/src/components/Home.jsx
@@ -10,7 +10,7 @@ function Home() {
         </div>
         <div className='home-content'>
             <h1 className='heading' style={{fontSize:"-webkit-xxx-large", textShadow: "0px 0px 90px #000, 0px 0px 35px #858585"}}>Hey User !!</h1>
-            <h5 className='sub-heading'>Beyond<span className='s-h'>, Play </span>&  Court</h5>
+            <h5 className='sub-heading'>Go Beyond<span className='s-h'>, Play </span>&  Court</h5>
             <h3 className='heading-content'>The beauty of the game is strength stamina and technique and when you shorten the game, technique goes for a toss and the quality of badminton will surely go down in this system.</h3>
             <div className='buttons'>
                 <Link to={'/game'}><button className='login' style={{padding:"10px 30px",boxShadow:"3px 3px 0px #000, 0px 0px 60px #858585"}}>Start</button></Link>
